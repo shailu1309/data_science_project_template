6ad321987ed3722362dfd6f18502a465a05df8f5
Shailaja Janjirala <shailaja.janjirala@sky.uk>
2024-10-13 15:03:03 +0100

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='data_science_project_template',
-    version='0.1.0',
+    version='0.2.0',
     description='Data Science Project Template',
     author='Shailaja Janjirala',
     packages=find_packages(),
