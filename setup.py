197fb58458c73eca4d2df4f9760379a49d432b2e
Shailaja Janjirala <shailaja.janjirala@sky.uk>
2024-10-13 15:13:40 +0100

    changes to workflow

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='data_science_project_template',
-    version='0.2.0',
+    version='0.2.2',
     description='Data Science Project Template',
     author='Shailaja Janjirala',
     packages=find_packages(),
